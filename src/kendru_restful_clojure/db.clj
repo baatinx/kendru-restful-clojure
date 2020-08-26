ad276b084936799f05ab6e6f4f4d6c34b5327cb0
Mustafa Basit <mailtobasit74@gmail.com>
2020-08-26 11:44:19 +0530

    part 4 pending - funcool/buddy

--- src/kendru_restful_clojure/db.clj
+++ src/kendru_restful_clojure/db.clj
@@ -1,2 +1,5 @@
 (ns kendru-restful-clojure.db)
-(def users (atom {1 {:name "Mustafa Basit" :age 23 :gender "m"}}))
+(def users (atom {1 {:name "Mustafa Basit" :age 24 :gender "m"}
+                  2 {:name "Hammad Mir" :age 20 :gender "m"}
+                  3 {:name "Anam Farooqui" :age 20 :gender "f"}
+                  4 {:name "Nasreen Sheikh" :age 20 :gender "f"}}))
---
+++ src/kendru_restful_clojure/query.clj
@@ -0,0 +1,6 @@
+(ns kendru-restful-clojure.query
+  (:require [kendru-restful-clojure.db :refer [users]]))
+
+(defn insert
+  [coll value]
+  )
